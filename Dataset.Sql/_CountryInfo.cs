634fe5dab4aba53906bc39890e2e2d2a3e4934d0
Berk Altun <vberkaltun@msn.com>
2020-03-31 15:40:48 +0200

    Bug fixes

--- Dataset.Sql/_CountryInfo.cs
+++ Dataset.Sql/_CountryInfo.cs
@@ -6,7 +6,6 @@
         public readonly string Province;
         public readonly string ISO2;
         public readonly string ISO3;
-
         public readonly double Latitude;
         public readonly double Longitude;
     }
